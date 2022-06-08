169593773d5d831e690dc0853168819a4d97ca81
Tammam Khan <tammam.a.khan@gmail.com>
2022-06-07 20:41:20 -0700

    added route to add new note

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,8 +1,24 @@
+const fs = require("fs");
+const path = require("path");
 const router = require("express").Router();
 const { notes } = require("../../db/notes");
+const { v4: uuidv4 } = require("uuid");
 
 router.get("/notes", (req, res) => {
   res.json(notes);
+});
+
+router.post("/notes", (req, res) => {
+  const note = req.body;
+  note._id = uuidv4();
+  notes.push(note);
+
+  fs.writeFileSync(
+    path.join(__dirname, "../../db/notes.json"),
+    JSON.stringify({ notes: notes })
+  );
+
+  res.json(note);
 });
 
 module.exports = router;
