84f910f8dc984718c73c4c5f1cc2517c64cfeebc
Tammam Khan <tammam.a.khan@gmail.com>
2022-06-07 20:53:24 -0700

    added route to delete a note

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -4,13 +4,15 @@ const router = require("express").Router();
 const { notes } = require("../../db/notes");
 const { v4: uuidv4 } = require("uuid");
 
+// Get all notes
 router.get("/notes", (req, res) => {
   res.json(notes);
 });
 
+// Post new note
 router.post("/notes", (req, res) => {
   const note = req.body;
-  note._id = uuidv4();
+  note.id = uuidv4();
   notes.push(note);
 
   fs.writeFileSync(
@@ -19,6 +21,20 @@ router.post("/notes", (req, res) => {
   );
 
   res.json(note);
+});
+
+// Delete a note by id
+router.delete("/notes/:id", (req, res) => {
+  let noteIndex = notes.findIndex((note) => note.id === req.params.id);
+
+  let deletedNote = notes.splice(noteIndex, 1);
+
+  fs.writeFileSync(
+    path.join(__dirname, "../../db/notes.json"),
+    JSON.stringify({ notes: notes })
+  );
+
+  res.json(deletedNote);
 });
 
 module.exports = router;
